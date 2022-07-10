c40d8091af7e16318ec63f28add12d83f3eb2d03
colaborador <colaborador@N091153.local>
2022-07-09 21:50:07 -0300

    estilizando card pokedex

--- pokedex7/src/components/CardPokedex/CardPokedex.js
+++ pokedex7/src/components/CardPokedex/CardPokedex.js
@@ -1,101 +1,70 @@
-import { CardMedia, Typography } from "@mui/material";
 import React, { useState } from "react";
 import {
   ConstainerControl,
-  ConstainerPokedex,
   ContainerCondicional,
+  ContainerPokedex,
+  ImagemContainer,
+  PokedexText,
 } from "./styled";
 
 export const CardPokedex = (props) => {
-  const { name, imageFront, imageBack, stats, types, moves } = props
+  const { name, imageFront, imageBack, stats, types, moves } = props;
   const [screen, setScreen] = useState(1);
 
   return (
-    <ConstainerPokedex>
+    <ContainerPokedex>
       <ContainerCondicional>
-        {screen === 1 && <CardMedia
-          component="img"
-          height="200"
-          width="200"
-          image={imageFront}
-          alt="green iguana"
-          sx={{ borderRaidus: "5px", backgroundColor: "#54661a" }}
-        />}
+        {screen === 1 && (
+          <ImagemContainer>
+            <img
+              src={imageFront}
+              alt={`imagem do pokemon ${name}`}
+              style={{ height: "300px", textAlign: "center" }}
+            ></img>
+          </ImagemContainer>
+        )}
 
-        {screen === 2 && <CardMedia
-          component="img"
-          height="200"
-          width="200"
-          image={imageBack}
-          alt="green iguana"
-          sx={{ borderRaidus: "5px", backgroundColor: "#54661a" }}
-        />}
+        {screen === 2 && (
+          <ImagemContainer>
+            <img
+              src={imageBack}
+              alt={`imagem do pokemon ${name}`}
+              style={{ height: "300px", textAlign: "center" }}
+            ></img>
+          </ImagemContainer>
+        )}
 
-        {screen === 3 && <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          color="black"
-          fontSize={"28px"}
-          textAlign={"center"}
-          margin={"5px"}
-          sx={{ backgroundColor: "white", opacity: "0.6" }}
-        >
-          Stats
-        </Typography>}
+        {screen === 3 && <PokedexText>Status</PokedexText>}
 
-        {screen === 3 && stats.map((stat) => {
-          return (
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              color="black"
-              fontSize={"28px"}
-              textAlign={"center"}
-              margin={"5px"}
-              sx={{ backgroundColor: "white", opacity: "0.6" }}
-            >
-              {`${stat.name}: ${stat.baseStat}`}
-            </Typography>
-          )
-        })}
+        {screen === 3 &&
+          stats.map((stat) => {
+            return (
+              <PokedexText>{`${stat.name}: ${stat.baseStat}`}</PokedexText>
+            );
+          })}
 
-        {screen === 4 && types.map((type) => {
-          return (
-            <Typography
-              key={`${name}-${type}`}
-              gutterBottom
-              variant="p"
-              component="div"
-              color="black"
-            >
-              {type}
-            </Typography>
-          )
-        })}
-        {screen === 5 && moves.map((move) => {
-          return (
-            <Typography
-              key={`${name}-${move}`}
-              gutterBottom
-              variant="p"
-              component="div"
-              color="black"
-            >
-              {move}
-            </Typography>
-          )
-        })}
+        {screen === 4 &&
+          types.map((type) => {
+            return <PokedexText key={`${name}-${type}`}>{type}</PokedexText>;
+          })}
+        {screen === 5 &&
+          moves.map((move) => {
+            return <PokedexText key={`${name}-${move}`}>{move}</PokedexText>;
+          })}
       </ContainerCondicional>
 
       <ConstainerControl>
-        <button onClick={() => setScreen(1)}>{"Frente"}</button>
+        <button
+          style={{ margin: "0 30px 0 30px" }}
+          onClick={() => setScreen(1)}
+        >
+          {"Frente"}
+        </button>
         <button onClick={() => setScreen(2)}>{"Costas"}</button>
         <button onClick={() => setScreen(3)}>{"Status"}</button>
         <button onClick={() => setScreen(4)}>{"Tipo"}</button>
         <button onClick={() => setScreen(5)}>{"Movimentos"}</button>
       </ConstainerControl>
-    </ConstainerPokedex>
+    </ContainerPokedex>
   );
 };
--- pokedex7/src/components/CardPokedex/styled.js
+++ pokedex7/src/components/CardPokedex/styled.js
@@ -1,13 +1,44 @@
+import { CardMedia } from "@mui/material";
 import styled from "styled-components";
 
-export const ConstainerPokedex = styled.div`
+export const ContainerPokedex = styled.div`
   background-color: red;
-  width: 800px;
-  height: 800px;
+  padding-top: 20px;
+  border-radius: 8px;
+  width: 500px;
+  height: 600px;
+  /* display: flex;
+  flex-direction: column;
+  align-items: center; */
 `;
 
-export const ConstainerControl = styled.div``;
+export const ConstainerControl = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 200px;
+  justify-items: center;
+  margin: auto;
+`;
 
 export const ContainerCondicional = styled.div`
+  margin: 30px;
   height: 300px;
+  /* object-fit: cover; */
+  background-color: #54661a;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+export const ImagemContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`;
+
+export const PokedexText = styled.p`
+  @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+  font-family: "Press Start 2P", cursive;
 `;
